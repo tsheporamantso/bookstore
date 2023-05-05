5c9ec5441014ab79e964624f8b25f15454f7ed3b
tsheporamantso <tshepo.ramantso@outlook.com>
2023-05-05 16:40:40 +0200

    Import form function

---
+++ src/pages/Books.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Book from '../components/Book';
+import Form from '../components/Form';
+
+function Books() {
+  return (
+    <div>
+      <Book title="Born a crime" author="Trevor Noah" />
+      <Form />
+    </div>
+  );
+}
+export default Books;
