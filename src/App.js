06c5186f79a6c2370dab8b3e01e3ef5ab94e2683
tsheporamantso <tshepo.ramantso@outlook.com>
2023-05-05 16:47:26 +0200

    Import categories function

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/Navbar';
+import Books from './pages/Books';
+import Categories from './pages/Categories';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Routes>
+        <Route path="/" element={<Books />} />
+        <Route path="/categories" element={<Categories />} />
+      </Routes>
     </div>
   );
 }
